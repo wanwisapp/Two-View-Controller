8623e0dcf5dbe0c2a4e889da959257cc8170b237
vimonrut <15nokmimee@gmail.com>
2016-06-23 11:31:29 +0700

    ฏนืำ 2 อรำไ

--- Two View Controller 2/ViewController.swift
+++ Two View Controller 2/ViewController.swift
@@ -20,6 +20,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
 
+    @IBAction func GotoSecond(sender: AnyObject) {
+        
+        let goToSecond = self.storyboard?.instantiateViewControllerWithIdentifier("wanwisappView") as! wanwisappViewController
+        self.navigationController?.pushViewController(goToSecond, animated: true)
+        
+        
+    }
 }
 
---
+++ Two View Controller 2/wanwisappViewController.swift
@@ -0,0 +1,35 @@
+//
+//  wanwisappViewController.swift
+//  Two View Controller 2
+//
+//  Created by iMac_25 on 6/23/2559 BE.
+//  Copyright © 2559 wanwisapp. All rights reserved.
+//
+
+import UIKit
+
+class wanwisappViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
